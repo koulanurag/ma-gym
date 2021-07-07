0cc5ad4f1e05631f0f74e60942a532958e415f24
Anurag Koul <koulanurag@gmail.com>
2021-07-06 20:10:29 -0700

    build: added exclusive "six" dependency

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(name='ma_gym',
           'pyglet>=1.4.0,<=1.5.0',
           'cloudpickle>=1.2.0,<1.7.0',
           'gym>=0.17.0',
+          'six>=1.16.0'
       ],
       extras_require=extras,
       tests_require=extras['test'],

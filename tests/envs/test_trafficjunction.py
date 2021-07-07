a94f157afce4bb0f78678eb2895c35d367ee94cf
Anurag Koul <koulanurag@gmail.com>
2021-07-06 22:56:40 -0700

    test: added rollouts for testing

--- tests/envs/test_trafficjunction.py
+++ tests/envs/test_trafficjunction.py
@@ -100,3 +100,52 @@ def test_step_cost_env10(env):
         target_reward = [env._step_cost * (step_i + 1) for _ in range(4)] + [0 for _ in range(env.n_agents - 4)]
         assert (reward_n == target_reward), \
             'step_cost is not correct. Expected {} ; Got {}'.format(target_reward, reward_n)
+
+
+@pytest_parametrize_plus('env',
+                         [fixture_ref(env_4)])
+def test_all_brake_rollout_env4(env):
+    """ All agent apply brake for the entire duration"""
+    for _ in range(2):
+        env.reset()
+        step_i = 0
+        done = [False for _ in range(env.n_agents)]
+        while not all(done):  # small number of steps so that no collision occurs
+            obs, reward_n, done, _ = env.step([1 for _ in range(env.n_agents)])
+            target_reward = [env._step_cost * (step_i + 1) for _ in range(env.n_agents)]
+            step_i += 1
+            assert (reward_n == target_reward), \
+                'step_cost is not correct. Expected {} ; Got {}'.format(target_reward, reward_n)
+    assert step_i == env._max_steps, 'max-steps should be reached'
+
+
+@pytest_parametrize_plus('env',
+                         [fixture_ref(env_4)])
+def test_one_gas_others_brake_rollout_env4(env):
+    """
+    "Agent 0" applies gas and others brake. This will mean that their will not be any collision and "Agent 0" will
+    reach it's destination in minimal number of steps; beyond which reward for agent "0" would be 0.
+    """
+
+    # testing over multiple episode to ensure it works with multiple routes assigned to "agent 0"
+    for episode_i in range(5):
+        obs = env.reset()
+        step_i = 0
+        done = [False for _ in range(env.n_agents)]
+        agent_0_route = obs[0].reshape((9, 9))[4][6:]  # one-hot
+        # Todo: Find max. steps for agent 0 based on it's route
+        max_agent_0_steps = 13 if all(agent_0_route == [0, 1, 0]) else 14
+        while not all(done):  # small number of steps so that no collision occurs
+            env.render()
+            obs, reward_n, done, _ = env.step([0] + [1 for _ in range(env.n_agents - 1)])
+            target_reward = [env._step_cost * (step_i + 1) for _ in range(env.n_agents)]
+            # once the car reaches destination, there is no step cost
+            if step_i >= max_agent_0_steps:
+                target_reward[0] = 0
+            step_i += 1
+            assert (reward_n == target_reward), \
+                'step_cost is not correct. Expected {} ; Got {}, Episode {} Agent 0 route:{} '.format(target_reward,
+                                                                                                      reward_n,
+                                                                                                      episode_i,
+                                                                                                      agent_0_route)
+    assert step_i == env._max_steps, 'max-steps should be reached'

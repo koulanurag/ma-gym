a0a664d5ea264c44414ab3d05399347e11d18bb5
Bryan <dongchirua@users.noreply.github.com>
2022-10-21 00:54:10 +1100

    fix: update pyglet (#45)
    fix error when rendering environment in a server 
    the error: `xvfb-run -s "-screen 0 1400x900x24"`

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras = {
 extras['all'] = [item for group in extras.values() for item in group]
 
 setup(name='ma_gym',
-      version='0.0.9',
+      version='0.0.10',
       description='A collection of multi agent environments based on OpenAI gym.',
       long_description_content_type='text/markdown',
       long_description=open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
@@ -24,7 +24,7 @@ setup(name='ma_gym',
       install_requires=[
           'scipy>=1.3.0',
           'numpy>=1.16.4',
-          'pyglet>=1.4.0,<=1.5.0',
+          'pyglet>=1.4.0,<=1.5.27',
           'cloudpickle>=1.2.0,<1.7.0',
           'gym==0.19.0',
           'pillow>=7.2.0',
